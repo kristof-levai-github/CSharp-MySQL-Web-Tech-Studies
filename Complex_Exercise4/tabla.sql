656e0dbcf25cc39d9b55d0e9f7256f93ad095d7d
kristof-levai <kristof.levai@gmail.com>
2023-08-07 22:13:51 +0200

    Add files via upload

---
+++ Complex_Exercise4/tabla.sql
@@ -0,0 +1,29 @@
+﻿
+
+
+
+CREATE TABLE `kiado` (
+ 
+  `id` int(2) NOT NULL,
+
+  `nev` varchar(48) DEFAULT NULL,
+  CONSTRAINT `pk_kiado` PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `film` (
+
+  `id` int(3) NOT NULL,
+
+  `cim` varchar(121) DEFAULT NULL,
+
+  `kiadasiev` int(11) DEFAULT NULL,
+
+  `kocka` int(11) DEFAULT NULL,
+
+  `szinese` tinyint(1) DEFAULT NULL,
+
+  `kiadoid` int(2) DEFAULT NULL,
+  CONSTRAINT `pk_film` PRIMARY KEY (`id`),
+  CONSTRAINT fk_filmkiado FOREIGN KEY (`kiadoid`) REFERENCES `kiado` (`id`)
+);
+
